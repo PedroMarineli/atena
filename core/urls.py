66c36bdfb5e5531fb3277ff6e45c98b188888100
PedroMarineli <pedrohenrique.marineli@gmail.com>
2026-01-02 14:53:19 -0300

    Remove dashboard URL routing from core URL configuration

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,6 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('dashboard/', include('dashboard.urls')),
     path('inventory/', include('inventory.urls')),
     path('sales/', include('sales.urls')),
     path('finance/', include('finance.urls')),
